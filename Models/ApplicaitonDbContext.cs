861015fa9e8215e2df992f6cad92279e3c8c7223
MDRezaulHasan <rezaulhasan0168@gmail.com>
2020-03-21 13:50:18 +0100

    Complete CRUD

---
+++ Models/ApplicaitonDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Web_app.Models
+{
+    public class ApplicaitonDbContext:DbContext
+    {
+        public ApplicaitonDbContext(DbContextOptions<ApplicaitonDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<NewEmpClass>EmployeeTable { get; set; }
+    }
+}
---
+++ Models/NewEmpClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_Web_app.Models
+{
+    public class NewEmpClass
+    {
+        [Key]
+        [Display(Name = "SL NO.")]
+
+        public int EmpId { get; set; }
+        
+        [Required(ErrorMessage ="Enter Employee Name")]
+        [Display(Name ="Employee Name")]
+        public string Empname { get; set; }
+
+        [Required(ErrorMessage = "Enter Email")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter Employee Age")]
+        [Display(Name = "Age")]
+        [Range(20,50)]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Enter Employee Salary")]
+        [Display(Name = "Salary")]
+        public int Salary { get; set; }
+
+    }
+}
